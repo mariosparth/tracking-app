00cd1a2e19ce63fd879a860396cb6d15eef2065c
Dimitra <marios.parthenios@gmail.com>
2018-02-13 07:51:10 +0200

    StackNavigator added

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -20,6 +20,7 @@ import { submitEntry, removeEntry } from "../utils/api";
 import { connect } from "react-redux";
 import { addEntry } from "../actions";
 import { purple, white } from "../utils/colors";
+import { NavigationActions } from "react-navigation";
 
 function SubmitBtn({ onPress }) {
   return (
@@ -80,7 +81,7 @@ class AddEntry extends Component {
 
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }));
 
-    // Navigate to home
+    this.toHome();
 
     submitEntry({ key, entry });
 
@@ -95,9 +96,12 @@ class AddEntry extends Component {
       })
     );
 
-    // Route to Home
+    this.toHome();
 
     removeEntry(key);
+  };
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({ key: "AddEntry" }));
   };
   render() {
     const metaInfo = getMetricMetaInfo();
