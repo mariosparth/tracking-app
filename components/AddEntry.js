e2302e7902004f8d535d378c500d98d7236a8068
Dimitra <marios.parthenios@gmail.com>
2018-02-21 11:15:51 +0200

    Clear daily localnotification if user has already submit values

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -9,7 +9,9 @@ import {
 import {
   getMetricMetaInfo,
   timeToString,
-  getDailyReminderValue
+  getDailyReminderValue,
+  clearLocalNotification,
+  setLocalNotification
 } from "../utils/helpers";
 import UdaciSlider from "./UdaciSlider";
 import UdaciSteppers from "./UdaciSteppers";
@@ -85,7 +87,7 @@ class AddEntry extends Component {
 
     submitEntry({ key, entry });
 
-    // Clear local notification
+    clearLocalNotification().then(setLocalNotification);
   };
   reset = () => {
     const key = timeToString();
